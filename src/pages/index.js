eae092a2959c61d1ddab785892fc4a3a6e043173
Stefan <stefan@elementsoftworks.co.uk>
2021-02-08 16:22:15 +0000

    GD-385 adding livestream section to the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import Winners from '@components/Winners';
 import { graphql, useStaticQuery } from 'gatsby';
 import SubscribeCTA from '@components/SubscribeCTA';
 import Container from '@components/Container';
+import Button from '@components/Button';
 
 const Home = () => {
     return (
@@ -17,6 +18,13 @@ const Home = () => {
             <Hero />
             <Winners />
             <Container>
+                <div className={styles.livestreamContainer}>
+                    <h3>Watch Last Year's Award Ceremony</h3>
+                    <Button
+                        text="Watch the Livestream on Youtube"
+                        link="https://www.youtube.com/watch?v=2_XL1LD9frc&feature=youtu.be"
+                    />
+                </div>
                 <div className={styles.ctaContainer}>
                     <h3>
                         Subscribe to our mailing list to be the first to know about Gundies 2022
